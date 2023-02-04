bbb8eb3ae7ed680179fa3f4e561cbb8cd5471327
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2023-02-04 20:16:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from Controle.classConexaoBD import ConexaoBD
+from Modelo.classLivros import Livros
+
 import mysql.connector
 
-from Controle.classConexao import Conexao
+def mostrarlivros(conexao):
 
+    lista_livros = conexao.consultarbanco('''
+    SELECT "ID", "Nome","autor","ano","categoria" FROM "Livros"
+    ORDE BY "ID" ASC  
+    ''')                                        #ORDE BY "ID" ASC  (serve para ordenar)
+    print ("ID | Nome          | Autor  ")
+    for liv in lista_livros:
+        print(f"{liv[0]} - {liv[1]} \n")
 
-
+teste = mostrarlivros(None)
