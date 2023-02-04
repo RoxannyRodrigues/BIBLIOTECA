4a4bc1fcbec41b9a132bc06f1abb2817acac20f9
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2023-02-04 20:16:31 -0300

    Create classLivros.py

---
+++ Modelo/classLivros.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+from Controle.classConexaoBD import *
+
+class Livros():
+    def __init__(self, id , nome, autor, ano, categoria):
+        self.id = id
+        self.nome = nome
+        self.autor = autor
+        self.ano = ano
+        self.categoria = categoria
+    
+    def incluir (self,tabela):
+        sql = f'''
+        INSERT INTO "{tabela}"
+        VALUES (default,'{self.nome}','{self.autor}',{self.ano}',{self.categoria} )'''
+
+        return sql
+    
+    def visualizar (self):
+
+        print(f'''
+        Informações do Livro:
+        ID - {self.id}
+        Nome - {self.nome}
+        Autor - {self.autor}
+        Ano - {self.ano}
+        Categoria - {self.categoria}''')
+    
+    def atualizarLivro(self,tabela):
+        sql = f'''
+        UPDATE "{tabela}"
+        SET "Nome" = '{self.nome}', "Autor" = '{self.autor}, "Ano" = '{self.Ano}', "Categoria" = '{self.categoria}'
+        WHERE "ID" = '`{self.id}'''
