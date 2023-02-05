7176cbcebe0fe08ee9b5e805d1101658d82667bb
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2023-02-04 21:15:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from Modelo.classLivros import Livros
 
 import mysql.connector
 
+try:
+    con = ConexaoBD("localhost", "root", "biblioteca")
+    print("Conectou")
+
+except(Exception,mysql.connector.Error) as error:
+    print("Ocorreu um erro:", error)
+
 def mostrarlivros(conexao):
 
     lista_livros = conexao.consultarBanco('''
-    SELECT "ID", "Nome","autor","ano","categoria" FROM "Livros"
-    ORDE BY "ID" ASC  
-    ''')                                        #ORDE BY "ID" ASC  (serve para ordenar)
-    print ("ID | Nome          | Autor  ")
+    SELECT id, nome, autor, ano, categoria FROM livros
+    ORDER BY id ASC
+    ''')                                        #ORDER BY "ID" ASC  (serve para ordenar)     # ORDER BY "id" ASC  
+    
     for liv in lista_livros:
-        print(f"{liv[0]} - {liv[1]} \n")
+        print(f"ID: {liv[0]} - Nome: {liv[1]} - Autor: {liv[2]} - Ano: {liv[3]} - Categoria: {liv[4]} \n")
 
-teste = mostrarlivros(None)
+teste = mostrarlivros(con)
