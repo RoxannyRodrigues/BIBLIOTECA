163eec2d2de0fcae4b37c6ba10aaadd3b0fa59b1
Roxanny Rodrigues <117939674+RoxannyRodrigues@users.noreply.github.com>
2023-02-24 09:48:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,115 +13,30 @@ except(Exception,mysql.connector.Error) as error:
     print("Ocorreu um erro:", error)
 
 
-#OPÇÕES DE LIVRO
-
-def mostrarlivros(conexao):
-
-    lista_livros = conexao.consultarBanco('''
-    SELECT * FROM biblioteca.livros;
-     ''')                                        #ORDER BY "ID" ASC  (serve para ordenar)     # ORDER BY "id" ASC  
-    
-    for liv in lista_livros:
-        print(f'ID: {liv[0]} - Nome: "{liv[1]}" - Autor: "{liv[2]}" - Categoria: "{liv[3]}" \n')
-
-def menuAlterarLivro (conexao):
-
-    print('''
-    1. Atualizar Livro
-    2. Remover Livro
-    ''')
-
-    opcoes = input("Digite o número da opção desejada: ")
-
-    match opcoes:
-        case "1":
-            livroEscolhido = input("Escolha o id do livro deseja alterar: ")    #Criar busca também por nome
-            livroConsulta = conexao.consultarBanco(f'''
-            SELECT * FROM livros
-            WHERE ID = {livroEscolhido}
+print("Bem-vindo a biblioteca")
+print('''Menu Principal:
+1. Gerenciar Clientes
+2. Gerenciar Livros
+3. Gerenciar Alugueis
+''')
+escolha = input("Digite: ")
+match escolha:
+    case "1":
+            print('''
+            1. Consultar CLiente
+            1. Cadastrar Cliente
+            2. Atualizar Cliente
+            3. Deletar Cliente
+            4. Voltar 
             ''')
-            opcoes = input('''
-            Seleciona o que você deseja alterar:
-            1 - Nome
-            2 - Autor
-            3 - Categoria 
-            Digite:
-            ''')
-            match opcoes:
-                case "1":
-                    
-                    novoNome = input("Digite o Novo Nome: ")
-                    atualizacaoescolhida = conexao.manipularBanco(f'''
-                    UPDATE livros SET nome = "{novoNome}" WHERE id = {livroEscolhido}''')
-                    print("Alterado com sucesso")
-                
-                case "2":
+            escolha1 = input("Digite: ")
+            match escolha1:
+                 case "1":
+                    escolha = input ("Digite id CLiente")
+                    cliente = Cliente(escolha, None, None, None)
+                    sql = cliente.consultarClientePorID()
+                    resultado = con.consultarBanco(sql)  
+                    print (f'{resultado}')
 
-                    novoAutor = input("Digite o Novo Autor: ")
-                    atualizacaoescolhida = conexao.manipularBanco(f'''
-                    UPDATE livros SET autor = "{novoAutor}" WHERE id = {livroEscolhido}''')
-                    print("Alterado com sucesso")
-                
-                case "3":
+                      
 
-                    novaCategoria = input("Digite a Nova Categoria: ")
-                    atualizacaoescolhida = conexao.manipularBanco(f'''
-                    UPDATE livros SET categoria = "{novaCategoria}" WHERE id = {livroEscolhido}''')
-                    print("Alterado com sucesso")                    
-
-            
-
-#OPÇÕES DE CLIENTES
-
-def mostrarClientes(conexao):
-    print("Lista de Cliente")
-    listacliente = conexao.consultarBanco('''
-    SELECT id, nome, cpf, limite de livros FROM cliente
-   
-    ''')
-
-    for cliente in listacliente:
-        print(f"ID: {cliente[0]} - Nome: '({cliente[1]})' - CPF: ({cliente[2]}) - Limite de Livros: ({cliente[3]}) \n")
-
-
-def menuAlterarClientes(conexao):
-    print(f'''
-    
-    Escolha uma das opções:
-    1. Buscar Cliente
-    2. Inserir novo Cliente
-    ''')
-    opcoes = input("Digite: ")
-    match opcoes:
-        case "1":
-            print(f'''
-            Buscar por:
-            1. ID
-            2. CPF
-            ''')
-            escolha = input("Digite: ")
-            match escolha:
-                case "1":
-                    iddocliente = input("Digite o ID: ")
-                    visualizacaocliente = conexao.consultarBanco(f'''
-                    SELECT * FROM cliente
-                    WHERE ID = {iddocliente}
-                    ''')
-                    for client in visualizacaocliente:
-                        print(f"ID: {client[0]} - Nome: {client[1]} - CPF: {client[2]} - Limite de Livros: {client[3]} \n")
-
-                case "2":
-                    cpfdocliente = input("Digite o CPF: ")
-                    visualizacaocliente = conexao.consultarBanco(f'''
-                    SELECT * FROM cliente
-                    WHERE cpf = {cpfdocliente}
-                    ''')
-                    for cpfclient in visualizacaocliente:
-                        print(f"ID: {cpfclient[0]} - Nome: {cpfclient[1]} - CPF: {cpfclient[2]} - Limite de Livros: {cpfclient[3]} \n")
-                        print(f'''
-                        Deseja atualizar Cliente:
-                        1. SIM
-                        2. NÃO
-                        ''')
-
-teste = mostrarClientes(con)
